955c6f319f069e27e13cec87b60cb794b44311eb
Raghav Khanna <37894186+raghavkhanna18@users.noreply.github.com>
2020-05-27 00:08:50 +0800

    Move onto correct git tree

---
+++ App.js
@@ -0,0 +1,114 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {Component, useState} from 'react';
+import LoginScreen from 'react-native-login-screen';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import set from '@babel/runtime/helpers/esm/set';
+
+const source = require('./resources/img/background.jpg');
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {userName: '', password: '', switchValue: true};
+  }
+
+  setSwitchValue(value) {
+    this.setState({switchValue: value});
+  }
+
+  setUsername(uN) {
+    this.setState({userName: uN});
+    console.log(this.state.userName);
+  }
+
+  setPassword(pass) {
+    this.setState({password:  pass});
+  }
+  getUsername() {
+    return this.state.userName;
+  }
+  getPassword() {
+    return this.state.password;
+  }
+  render() {
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <LoginScreen
+            logoText="Swift Staff"
+            source={source}
+            switchValue={this.state.switchValue}
+            onPressLogin={() => alert('Login Button is pressed')}
+            onPressSettings={() => alert('Settings Button is pressed')}
+            onSwitchValueChange={sV => this.setSwitchValue(sV)}
+            usernameOnChangeText={username => this.setUsername(username)}
+            passwordOnChangeText={password => this.setPassword(password)}
+            loginButtonBackgroundColor="#a2a5a9"
+          />
+        </SafeAreaView>
+      </>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
