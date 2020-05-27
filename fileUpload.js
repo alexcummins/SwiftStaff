2bfbf4be9cb96baa99e3fda93228cbc2f6895171
Raghav Khanna <37894186+raghavkhanna18@users.noreply.github.com>
2020-05-28 03:23:24 +0800

    Fix file path for CD using deploy stage and run in one stage

--- fileUpload.js
+++ fileUpload.js
@@ -6,16 +6,22 @@ const fs = require('fs');
   const dropbox = dropboxV2Api.authenticate({
     token: AUTH_TOKEN,
   });
-
-  dropbox({
-    resource: 'files/upload',
-    parameters: {
-      path: `/android/app-release_${new Date().toUTCString()}.apk`,
-    },
-    readStream: fs.createReadStream('android/app/build/outputs/apk/release/app-release.apk'),
-  }, (err, result, response) => {
-    if(err) {
-      console.log(`Failed to upload ${JSON.stringify(response)}`);
-    }
-  });
+  let filePath = './android/app/build/outputs/apk/release/app-release.apk';
+  if (!fs.existsSync(filePath)) {
+    console.log("File missing. Exiting")
+    process.exit(1);
+  } else {
+    dropbox({
+      resource: 'files/upload',
+      parameters: {
+        path: `/android/app-release_${new Date().toUTCString()}.apk`,
+      },
+      readStream: fs.createReadStream(filePath),
+    }, (err, result, response) => {
+      if (err) {
+        console.log(`Failed to upload ${JSON.stringify(response)}`);
+        process.exit(1);
+      }
+    });
+  }
 })();
