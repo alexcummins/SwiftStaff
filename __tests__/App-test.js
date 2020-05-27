9112fb2cf8ae6150202bf32ef40dcaa2a695d667
Raghav Khanna <37894186+raghavkhanna18@users.noreply.github.com>
2020-05-27 08:07:07 +0800

    Fix Jest tests to transpile all node modules

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -9,6 +9,7 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+it('Spinner Should be enable initially', () => {
+  const app = new App();
+  expect(app.state.spinnerEnable).toBe(true);
 });
---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  "transformIgnorePatterns": [
+    // Change MODULE_NAME_HERE to your module that isn't being compiled
+    "/node_modules/\\.js$"
+  ]
+}
