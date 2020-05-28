e6d8a76b41d8afa323832c270b8890d7d1bdba2f
Raghav Khanna <37894186+raghavkhanna18@users.noreply.github.com>
2020-05-29 05:16:38 +0800

    Fix broken App.js

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React, {Component, useState} from 'react';
 import LoginScreen from 'react-native-login-screen';
-import StatusBar from 'react-native';
+import {StatusBar} from 'react-native';
 
 
 const source = require('./resources/img/background.jpg');
+import set from '@babel/runtime/helpers/esm/set';
 
 export default class App extends Component {
   constructor(props) {
