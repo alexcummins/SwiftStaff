71983063d35438f4f476866a6e09fe5aa53eedd0
Raghav Khanna <37894186+raghavkhanna18@users.noreply.github.com>
2020-06-03 07:42:20 +0800

    Implement a logout feature on home screen as requested on feedback by Vishal.

--- src/screens/Worker/TempWorkerHomeScreen.js
+++ src/screens/Worker/TempWorkerHomeScreen.js
@@ -1,15 +1,30 @@
 import {Text, View} from 'react-native';
-import styles from 'react-native-login-screen/lib/src/components/Logo/Logo.style';
-import Icon from 'react-native-dynamic-vector-icons/lib/components/Icon';
-import { Avatar } from 'react-native-paper';
+import {Avatar, Button} from 'react-native-paper';
 import React from 'react';
+import {CommonActions, useNavigation} from '@react-navigation/native';
 
 export default function TempWorkerHomeScreen() {
 
+  const navigation = useNavigation();
+
+  function logout(){
+    navigation.dispatch(
+      CommonActions.reset({
+        index: 0,
+        routes: [
+          { name: 'Login' }
+        ],
+      }));
+  }
 
   return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }} >
         <Text>SwiftStaff</Text>
+        <Button
+          icon="logout"
+          mode="contained"
+          onPress={() => logout()}
+        >Logout</Button>
       </View>
   )
 }
