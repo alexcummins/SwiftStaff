b775dfbf81e789051f6d642b283795eda5f52126
Alex <47060235+alexcummins@users.noreply.github.com>
2020-06-03 02:17:51 +0100

    Create text fields for signup form, change header title in navigation

--- App.js
+++ App.js
@@ -14,88 +14,89 @@ import RestaurantOrWorkerSignup from "./src/screens/signup/RestaurantOrWorkerSig
 const Stack = createStackNavigator();
 
 export default function App({navigator}) {
-  const [loading, setLoading] = useState(true);
-  const [initialRoute, setInitialRoute] = useState('Home');
+    const [loading, setLoading] = useState(true);
+    const [initialRoute, setInitialRoute] = useState('Home');
 
-  useEffect(() => {
-    (async () => {
-      await registerAppWithFCM();
-      await requestPermission();
-    })();
-
-
-    firebase.messaging().onNotificationOpenedApp(remoteMessage => {
-      console.log(
-        'Notification caused app to open from background state:',
-        remoteMessage.notification,
-      );
-      if (remoteMessage.data.type === 'User' || remoteMessage.data.type === 'Restaurant') {
-        navigate(remoteMessage.data.type);
-      }
-      Alert.alert(
-        'Notification caused app to open from background state:',
-        JSON.stringify(remoteMessage),
-      );
-    });
-
-    // Check whether an initial notification is available
-    firebase.messaging()
-      .getInitialNotification()
-      .then(remoteMessage => {
-        if (remoteMessage) {
-          console.log(
-            'Notification caused app to open from quit state:',
-            remoteMessage.notification,
-          );
-          if (remoteMessage.data.type === 'User' || remoteMessage.data.type === 'Restaurant') {
-            setInitialRoute(remoteMessage.data.type);
-          }
-        }
-        setLoading(false);
-      });
-    const unsubscribe = firebase.messaging().onMessage(async remoteMessage => {
-      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
-    });
-
-    return unsubscribe;
-  }, []);
+    useEffect(() => {
+        (async () => {
+            await registerAppWithFCM();
+            await requestPermission();
+        })();
 
 
-  async function registerAppWithFCM() {
-    console.log('registering');
-    await firebase.messaging().registerDeviceForRemoteMessages();
-  }
+        firebase.messaging().onNotificationOpenedApp(remoteMessage => {
+            console.log(
+                'Notification caused app to open from background state:',
+                remoteMessage.notification,
+            );
+            if (remoteMessage.data.type === 'User' || remoteMessage.data.type === 'Restaurant') {
+                navigate(remoteMessage.data.type);
+            }
+            Alert.alert(
+                'Notification caused app to open from background state:',
+                JSON.stringify(remoteMessage),
+            );
+        });
 
-  async function requestPermission() {
-    const authStatus = await firebase.messaging().requestPermission({
-      alert: true,
-      announcement: false,
-      badge: true,
-      carPlay: false,
-      provisional: false,
-      sound: true,
-    });
-    if (authStatus === 1 || authStatus === 2) {
-      const fcmToken = await firebase.messaging().getToken();
-      console.log(fcmToken);
-      console.log('Authorization status:', authStatus);
-    } else {
-      console.log('User declined messaging permission', authStatus);
+        // Check whether an initial notification is available
+        firebase.messaging()
+            .getInitialNotification()
+            .then(remoteMessage => {
+                if (remoteMessage) {
+                    console.log(
+                        'Notification caused app to open from quit state:',
+                        remoteMessage.notification,
+                    );
+                    if (remoteMessage.data.type === 'User' || remoteMessage.data.type === 'Restaurant') {
+                        setInitialRoute(remoteMessage.data.type);
+                    }
+                }
+                setLoading(false);
+            });
+        const unsubscribe = firebase.messaging().onMessage(async remoteMessage => {
+            Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+        });
+
+        return unsubscribe;
+    }, []);
+
+
+    async function registerAppWithFCM() {
+        console.log('registering');
+        await firebase.messaging().registerDeviceForRemoteMessages();
     }
-  }
 
-  if (loading) {
-    return null;
-  }
+    async function requestPermission() {
+        const authStatus = await firebase.messaging().requestPermission({
+            alert: true,
+            announcement: false,
+            badge: true,
+            carPlay: false,
+            provisional: false,
+            sound: true,
+        });
+        if (authStatus === 1 || authStatus === 2) {
+            const fcmToken = await firebase.messaging().getToken();
+            console.log(fcmToken);
+            console.log('Authorization status:', authStatus);
+        } else {
+            console.log('User declined messaging permission', authStatus);
+        }
+    }
 
-  return (
+    if (loading) {
+        return null;
+    }
+
+    return (
         <Stack.Navigator>
-          <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
-          <Stack.Screen name="HomeRestaurant" component={RestaurantScreens} options={{headerShown: false}}/>
-          <Stack.Screen name="HomeTempWorker" component={TempWorkerScreens} options={{headerShown: false}}/>
-          <Stack.Screen name="RestaurantOrWorkerSignup" component={RestaurantOrWorkerSignup}/>
+            <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
+            <Stack.Screen name="HomeRestaurant" component={RestaurantScreens} options={{headerShown: false}}/>
+            <Stack.Screen name="HomeTempWorker" component={TempWorkerScreens} options={{headerShown: false}}/>
+            <Stack.Screen name="RestaurantOrWorkerSignup" component={RestaurantOrWorkerSignup}
+                          options={{title: 'Sign up'}}/>
         </Stack.Navigator>
-  )
+    )
 }
 
 
--- src/screens/signup/RestaurantOrWorkerSignup.js
+++ src/screens/signup/RestaurantOrWorkerSignup.js
@@ -1,34 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 import {StyleSheet, View} from "react-native";
-import {Button, Paragraph, Title} from 'react-native-paper';
+import {Button, Paragraph, Text, TextInput, Title} from 'react-native-paper';
 
-export default function RestaurantOrWorkerSignup({ navigation }) {
+export default function RestaurantOrWorkerSignup({navigation}) {
+
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [confirmPassword, setConfirmPassword] = useState();
 
     return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Title>Pick Restaurant or worker</Title>
-        <Button
-            icon="food"
-            mode="contained"
-            onPress={() => console.log("pressed")}
-        >
-            Restaurant
-        </Button>
-        <Paragraph></Paragraph>
-        <Button
-            icon="worker"
-            mode="contained"
-            onPress={() => console.log("pressed")}
-        >
-            Worker
-        </Button>
-    </View>
+        <View style={{ margin: 5, justifyContent: 'center'}}>
+            <TextInput
+                mode='outlined'
+                label={"Email"}
+                value={email}
+                onChangeText={email => setEmail(email)}
+                textContentType="username"
+                keyboardType="email-address"
+            >
+            </TextInput>
+            <TextInput
+                mode='outlined'
+                label={"Password"}
+                value={password}
+                onChangeText={password => setPassword(password)}
+                textContentType="password"
+            >
+            </TextInput>
+            <TextInput
+                mode='outlined'
+                label={"Confirm Password"}
+                value={confirmPassword}
+                onChangeText={confirmPassword => setConfirmPassword(confirmPassword)}
+            >
+            </TextInput>
+        </View>
     );
 }
-
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-});
