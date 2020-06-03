b3859b49404a81d2278e5cf654713fe9b36d0ea6
TerenceHernandez <t.na.m.hernandez@gmail.com>
2020-06-03 12:01:00 +0100

    StylesSheet refactor, refactored styles from View component

--- src/screens/PickRestaurantOrWorkerScreen.js
+++ src/screens/PickRestaurantOrWorkerScreen.js
@@ -5,7 +5,7 @@ import {Title, Button, Paragraph} from 'react-native-paper';
 
 export default function PickRestaurantOrWorkerScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View style={styles.topView}>
       <Title>Pick Restaurant or worker</Title>
       <Button
         icon="food"
@@ -26,9 +26,13 @@ export default function PickRestaurantOrWorkerScreen({ navigation }) {
   );
 }
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+    topView: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    }
 });
