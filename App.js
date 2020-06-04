7f63735e2ace84ad0211b70042e6e046cce36742
TerenceHernandez <t.na.m.hernandez@gmail.com>
2020-06-04 14:58:49 +0100

    Undo Merging artefact: HomeRestaurant title name changed to Back

--- App.js
+++ App.js
@@ -92,7 +92,7 @@ export default function App({navigator}) {
     return (
         <Stack.Navigator>
             <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
-            <Stack.Screen name="HomeRestaurant" component={RestaurantScreens} options={{headerShown: false}}/>
+            <Stack.Screen name="HomeRestaurant" component={RestaurantScreens} options={{headerShown: false, title: 'Back'}}/>
             <Stack.Screen name="HomeTempWorker" component={TempWorkerScreens} options={{headerShown: false}}/>
             <Stack.Screen name="RestaurantOrWorkerSignup" component={RestaurantOrWorkerSignup}
                           options={{title: 'Sign up'}}/>
