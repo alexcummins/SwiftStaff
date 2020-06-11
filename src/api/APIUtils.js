1498c39f5f92493a64339d6a5e5a807bc989c777
TerenceHernandez <t.na.m.hernandez@gmail.com>
2020-06-11 22:55:10 +0100

    PUT API request for udating worker ratings

--- src/api/APIUtils.js
+++ src/api/APIUtils.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import FormData from "form-data";
 
-export const API_BASE_URL = '139.59.200.194:8080/api/v1';
-// export const API_BASE_URL = 'localhost:8080/api/v1';
+// export const API_BASE_URL = '139.59.200.194:8080/api/v1';
+export const API_BASE_URL = 'localhost:8080/api/v1';
 export const API_JOB_URL = `${API_BASE_URL}/jobs`;
 export const API_WORKER_JOB_URL = `${API_BASE_URL}/jobs/worker`;
 export const API_WORKER_SIGNUP_URL = `${API_BASE_URL}/signup/worker`;
@@ -11,6 +11,7 @@ export const HTTP_PROTOCOL = 'http://';
 export const WEBSOCKET_PROTOCOL = 'ws://';
 export const API_LOGIN_URL = `${API_BASE_URL}/login`;
 export const API_PROFILE_WORKER = `${API_BASE_URL}/profile/worker`
+export const API_NEW_RATING_WORKER = `${API_BASE_URL}/new/rating/worker`
 export const API_IMAGE_UPLOAD = `${API_BASE_URL}/uploads`
 export const API_IMAGE_DOWNLOAD = `${API_BASE_URL}/downloads`
 export const API_IMAGE_DOWNLOAD_URI = `${HTTP_PROTOCOL}${API_BASE_URL}`
@@ -37,6 +38,13 @@ export async function getLoginRequest(params) {
 
 export async function getWorkerProfile(params) {
     let response = await sendHttpPostRequest(params, API_PROFILE_WORKER)
+    if (response.status === 200) {
+        return response.data
+    }
+}
+
+export async function sendNewWorkerRating(params) {
+    let response = await sendHttpPutRequest(params, API_NEW_RATING_WORKER)
     if (response.status === 200) {
         return response.data
     }
@@ -93,6 +101,19 @@ async function sendHttpPatchRequest(data, url){
 async function sendHttpPostRequest(data, url, headers : any = dontStoreCache) {
     let responseObject = {}
     await axios.post(`${HTTP_PROTOCOL}${url}`, data, headers).then( (response) => {
+        console.log(JSON.stringify(response))
+        responseObject = response
+    }).catch( (error) => {
+        console.log(JSON.stringify(error))
+        responseObject = error.response
+    });
+    return responseObject
+}
+
+async function sendHttpPutRequest(data, url, headers : any = dontStoreCache) {
+    let responseObject = {}
+    console.log(url)
+    await axios.put(`${HTTP_PROTOCOL}${url}`, data, headers).then( (response) => {
         console.log(JSON.stringify(response))
         responseObject = response
     }).catch( (error) => {
